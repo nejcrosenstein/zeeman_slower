9f8fba2eaa283ddaf2bf795986778cf1d118131e
nejc <nejc.pinkstone@gmail.com>
2018-03-01 23:42:42 +0100

    XoroshiroSIMD: for scalar operations, use implementations provided by the authors of the algorithm, because it makes no sense to have the same routines defined twice. Also did small mofications to original implementations without altering the algorithm.

--- externals/randgen/xoroshiro128plus.hpp
+++ externals/randgen/xoroshiro128plus.hpp
@@ -43,8 +43,6 @@
 // **************************************************************
 
 
-#if 0
-
 /*  Written in 2016 by David Blackman and Sebastiano Vigna (vigna@acm.org)
 
 To the extent possible under law, the author has dedicated all copyright
@@ -79,13 +77,14 @@ The state must be seeded so that it is not everywhere zero. If you have
 a 64-bit seed, we suggest to seed a splitmix64 generator and use its
 output to fill s. */
 
-uint64_t s[2];
+namespace xoroshiro_original
+{
 
-static inline uint64_t rotl(const uint64_t x, int k) {
+static uint64_t rotl(const uint64_t x, int k) {
   return (x << k) | (x >> (64 - k));
 }
 
-uint64_t next(void) {
+static uint64_t next(uint64_t(&s)[2]) {
   const uint64_t s0 = s[0];
   uint64_t s1 = s[1];
   const uint64_t result = s0 + s1;
@@ -94,7 +93,7 @@ uint64_t next(void) {
   s[0] = rotl(s0, 55) ^ s1 ^ (s1 << 14); // a, b
   s[1] = rotl(s1, 36); // c
 
-  return result;
+   return result;
 }
 
 
@@ -102,25 +101,30 @@ uint64_t next(void) {
 to 2^64 calls to next(); it can be used to generate 2^64
 non-overlapping subsequences for parallel computations. */
 
-void jump(void) {
+static void jump(uint64_t(&s)[2]) {
   static const uint64_t JUMP[] = { 0xbeac0467eba5facb, 0xd86b048b86aa9922 };
 
   uint64_t s0 = 0;
   uint64_t s1 = 0;
   for (int i = 0; i < sizeof JUMP / sizeof *JUMP; i++)
+  {
     for (int b = 0; b < 64; b++) {
       if (JUMP[i] & UINT64_C(1) << b) {
         s0 ^= s[0];
         s1 ^= s[1];
       }
-      next();
+      next(s);
     }
+  }
 
   s[0] = s0;
   s[1] = s1;
 }
 
-#endif
+}
+
+  
+
 
 
 // ******************************************************************
@@ -243,10 +247,6 @@ struct XoroshiroSIMD
              _mm256_srli_epi64(x, 64 - k));
   }
 
-  static inline uint64_t rotl(const uint64_t x, int k) {
-    return (x << k) | (x >> (64 - k));
-  }
-
   __m256i generate_simd() 
   {  
     const __m256i s0 = state_[0];
@@ -266,39 +266,6 @@ struct XoroshiroSIMD
     return result;
   }
 
-  uint64_t generate_scalar(uint64_t& state0, uint64_t& state1) {
-    const uint64_t s0 = state0;
-    uint64_t s1 = state1;
-    const uint64_t result = s0 + s1;
-
-    s1 ^= s0;
-    state0 = rotl(s0, 55) ^ s1 ^ (s1 << 14); // a, b
-    state1 = rotl(s1, 36); // c
-
-    return result;
-  }
-
-
-  /* This is the jump function for the generator. It is equivalent
-  to 2^64 calls to next(); it can be used to generate 2^64
-  non-overlapping subsequences for parallel computations. */
-  void jump(uint64_t& state0, uint64_t& state1) {
-    static const uint64_t JUMP[] = { 0xbeac0467eba5facb, 0xd86b048b86aa9922 };
-
-    uint64_t s0 = 0;
-    uint64_t s1 = 0;
-    for (int i = 0; i < sizeof JUMP / sizeof *JUMP; i++)
-      for (int b = 0; b < 64; b++) {
-        if (JUMP[i] & UINT64_C(1) << b) {
-          s0 ^= state0;
-          s1 ^= state1;
-        }
-        this->generate_scalar(state0, state1);
-      }
-
-    state0 = s0;
-    state1 = s1;
-  }
 
   void jump(int offset, int factor)
   {
@@ -309,10 +276,15 @@ struct XoroshiroSIMD
     
     for (int j = 0; j < 4; ++j)
     {
+      uint64_t curr_state[2] = { s0[j], s1[j] };
+
       for (int i = 0; i < offset + j*factor; ++i)
       {
-        jump(s0[j], s1[j]);
+        xoroshiro_original::jump(curr_state);
       }
+
+      s0[j] = curr_state[0];
+      s1[j] = curr_state[1];
     }
 
     state_[0] = _mm256_load_si256((__m256i*)&s0[0]);
